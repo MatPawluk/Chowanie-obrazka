73890b48ce04c9db150f6cdb615b7d9e27aa3393
Mateusz Pawluk <pawluk1988@icloud.com>
2024-08-27 19:10:02 +0200

    ciekawostki

--- app.js
+++ app.js
@@ -1,54 +1,23 @@
-const convBtn = document.querySelector(".conv");
-const resetBtn = document.querySelector(".reset");
-const changeBtn = document.querySelector(".change");
-const converter = document.querySelector("#converter");
-const result = document.querySelector(".result");
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
+const currentDay = document.querySelector(".current-day");
+const funFact = document.querySelector(".fun-fact");
 
-let fahrenheit;
-let celsius;
+const facts = [
+  "Krokodyl nie potrafi wystawić języka.",
+  "Każdy człowiek spędził około pół godziny jako pojedyncza komórka.",
+  "Dźwięk przemieszcza się 15 razy szybciej przez stal niż przez powietrze.",
+  "Leniwce potrzebują dwóch tygodni na strawienie jedzenia.",
+  "Goryle śpią nawet czternaście godzin dziennie.",
+  "Język kameleona jest dwukrotnie dłuższy od jego ciała.",
+  "Chińczycy w ciągu roku zużywają około 80 miliardów pałeczek.",
+  "Żeby wejść na Wieżę Eiffla trzeba pokonać aż 1710 stopni.",
+];
 
-const change = () => {
-  if (one.textContent.includes("°C")) {
-    one.textContent = "°F";
-    two.textContent = "°C";
-  } else {
-    one.textContent = "°C";
-    two.textContent = "°F";
-  }
-  result.textContent = "";
+const day = new Date();
+currentDay.textContent = day.toLocaleDateString("pl", { weekday: "long" });
+
+const showRandomFact = () => {
+  const number = Math.floor(Math.random() * facts.length);
+  funFact.textContent = facts[number];
 };
 
-const celsiusToFahrenheit = () => {
-  fahrenheit = converter.value * 1.8 + 32;
-  result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`;
-  converter.value = "";
-};
-
-const fahrenheitToCelsius = () => {
-  celsius = (converter.value - 32) / 1.8;
-  result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`;
-  converter.value = "";
-};
-
-const conversion = () => {
-  if (converter.value !== "") {
-    if (one.textContent.includes("°C")) {
-      celsiusToFahrenheit();
-    } else {
-      fahrenheitToCelsius();
-    }
-  } else {
-    result.textContent = "Podaj wartość";
-  }
-};
-
-const restart = () => {
-  result.textContent = "";
-  converter.value = "";
-};
-
-changeBtn.addEventListener("click", change);
-convBtn.addEventListener("click", conversion);
-resetBtn.addEventListener("click", restart);
+showRandomFact();
